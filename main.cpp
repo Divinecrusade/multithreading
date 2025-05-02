499a030798b949c113f1a988acacdbe7341aaef1
DivineCrusade <gorlovdima@inbox.ru>
2025-05-02 10:23:48 +0300

    refactor: Remove unnecessary total sum

--- main.cpp
+++ main.cpp
@@ -164,27 +164,23 @@ static void process_data_in_multi_shots(std::vector<DUMMY_DATA> const& data)
 
     for (int i{ 1 }; i <= ITERATIONS_COUNT; ++i)
     {
-        ACC total{ 0ll };
+        std::array<ACC_PADDED, BLOCKS_DATA_COUNT> accs{ };
 
         auto const start_time{ std::chrono::steady_clock::now() };
 
         constexpr std::size_t CHUNK_SIZE{ DUMMY_DATA_SIZE / 1'000ull };
         for (std::size_t j{ 0ull }; j != DUMMY_DATA_SIZE; j += CHUNK_SIZE)
         {
-            std::array<ACC_PADDED, BLOCKS_DATA_COUNT> accs{ };
-            
             for (std::vector<std::jthread> workers{ }; auto const& [i, block] : std::views::enumerate(data))
             {
                 workers.push_back(std::jthread{ dummy_process_multi, std::span{ block.begin() + j, CHUNK_SIZE }, std::ref(accs[i].first)});
             }
-
-            total += std::accumulate(accs.cbegin(), accs.cend(), static_cast<ACC>(0), [](auto const& lhs, auto const& rhs) -> ACC { return lhs + rhs.first; });
         }
 
         auto const end_time{ std::chrono::steady_clock::now() };
 
         std::cout << "Done in " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << " ms (4 threads with padding)\n";
-        std::cout << "Result: " << total;
+        std::cout << "Result: " << std::accumulate(accs.cbegin(), accs.cend(), static_cast<ACC>(0), [](auto const& lhs, auto const& rhs) -> ACC { return lhs + rhs.first; });
         std::puts("");
     }
     std::puts("=========================");
